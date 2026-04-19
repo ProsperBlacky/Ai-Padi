2cfd011cf976ca2ce7717cd463ea2a48da3d5b16
ProsperBlacky <95464048+ProsperBlacky@users.noreply.github.com>
2026-04-19 18:30:33 +0100

    Update page.js

--- app/result/page.js
+++ app/result/page.js
@@ -16,16 +16,29 @@ export default function Result() {
   };
 
   return (
-    <div style={{ padding: 30 }}>
-      <h2>Your Result</h2>
+    <div style={{ padding: 20 }}>
+      <h2>Your Skill Report</h2>
 
-      <pre style={{ whiteSpace: "pre-wrap" }}>
-        {result}
-      </pre>
+      <div style={{
+        background: "#fff",
+        padding: 20,
+        borderRadius: 10,
+        marginTop: 20
+      }}>
+        <pre style={{ whiteSpace: "pre-wrap" }}>
+          {result}
+        </pre>
+      </div>
 
-      <br />
-
-      <button onClick={download}>
+      <button
+        onClick={download}
+        style={{
+          marginTop: 20,
+          background: "#F05A28",
+          color: "#fff",
+          padding: 10
+        }}
+      >
         Download PDF
       </button>
     </div>
